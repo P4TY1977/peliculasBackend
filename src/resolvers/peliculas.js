e2f0a33e9adebd75695407243d802f1d0bd6cee7
Patricia Medina <f.patricia.medina@gmail.com>
2022-04-22 15:44:35 -0500

    carga inicial

---
+++ src/resolvers/peliculas.js
@@ -0,0 +1,9 @@
+const listar = async ({dataSources})=>
+{
+    const peliculas = await dataSources.peliculas.listar()
+
+    if (!peliculas)
+        return {success: false}
+    return {success: true, peliculas}
+}
+module.exports ={listar}

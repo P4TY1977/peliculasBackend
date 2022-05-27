3757f2a1868366d1d5afd3e20598db8df0eafcc9
Patricia Medina <f.patricia.medina@gmail.com>
2022-05-27 18:20:16 -0500

    peliculas-test -> peliculas_test

--- src/config.js
+++ src/config.js
@@ -3,7 +3,6 @@ module.exports=
    MYSQL_HOSTNAME: process.env.MYSQL_HOSTNAME || 'localhost',
    MYSQL_USER    : process.env.MYSQL_USER     || 'root',
    MYSQL_PASSWORD: process.env.MYSQL_PASSWORD || 'g@t0P4to',
-   MYSQL_DATABASE: process.env.MYSQL_DATABASE || 'peliculas-test',
-   MYSQL_DATABASE: process.env.MYSQL_DATABASE || 'peliculas-test',
+   MYSQL_DATABASE: process.env.MYSQL_DATABASE || 'peliculas_test',
    MYSQL_CONNECTION_LIMIT: process.env.MYSQL_CONNECTION_LIMIT|| 10
 }

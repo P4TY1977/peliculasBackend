5d71a6d1d3200d61979fd9a665fc3b72cdca78ce
Patricia Medina <f.patricia.medina@gmail.com>
2022-05-27 18:30:16 -0500

    se elimino collate de schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -4,4 +4,4 @@ CREATE TABLE `peliculas` (
   `genero` varchar(100) DEFAULT NULL,
   `clasificacion` varchar(5) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4

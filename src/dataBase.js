449f091414a8030346b241dbcb78ce109c974976
Patricia Medina <f.patricia.medina@gmail.com>
2022-05-27 18:39:08 -0500

    cambio sin importancia

--- src/dataBase.js
+++ src/dataBase.js
@@ -12,7 +12,7 @@ async function init()
         user: config.MYSQL_USER,
         password: config.MYSQL_PASSWORD,
         database: config.MYSQL_DATABASE
-    })
+    }) 
 }
 
 async function withPool(callback)
